791531802bb7aebe3836d5b56db5f1c4aa1fc92e
crousekimo <43980978+crousekimo@users.noreply.github.com>
2023-08-04 23:53:25 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# MicrobittoJQ8400
+
+Micro:Bit控制JQ8400積木
+作者：蔡佳倫(Jia-Lun Tsai)
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+pxt-jq8400=github:crousekimo/pxt-jq8400
+```
---
+++ main.ts
@@ -0,0 +1,193 @@
+//% weight=0 color=#338899 icon="\uf0ad" block="microbittojq8400"
+namespace microbittojq8400 {
+     export enum play_mode {
+        //% block="全部循環"
+        ALL_REPEAT = 1 ,
+        //% block="單曲循環"
+        SINGLE_REPEAT = 2,
+        //% block="單曲暫停"
+        SINGLE_STOP = 3,
+        //% block="隨機撥放"
+        RANDOM = 4,
+        //% block="順序撥放"
+        ORDER_PLAY = 5
+     }
+     export enum mp3_mode {
+          //% block="前進"
+          fast_forward =1,
+          //% block="後退"
+          rewind=2
+     }
+    //% blockId=jq8400_setMicrobit block="Microbitc | TX %tx | RX %rx"
+    //% tx.defl=SerialPin.P0
+    //% rx.defl=SerialPin.P1
+    //% weight=120
+    export function jq8400_setMicrobit(tx: SerialPin, rx: SerialPin) {
+        serial.redirect(
+            tx,
+            rx,
+            9600
+        )
+        basic.pause(100)
+    }
+
+    //% blockId=playcurrentmp3 block="JQ8400 play current mp3"
+    //% weight=100
+    export function playcurrentmp3() {
+            let _a=pins.createBuffer(4)
+    	       _a[0] = 170
+            _a[1] = 2
+            _a[2] = 0
+            _a[3] = 172
+    	       serial.writeBuffer(_a)
+    }    
+    //% blockId=pausemp3 block="JQ8400 pause mp3"
+    //% weight=96
+    export function pausemp3() {
+            let _a=pins.createBuffer(4)
+    	       _a[0] = 170
+            _a[1] = 3
+            _a[2] = 0
+            _a[3] = 173
+    	       serial.writeBuffer(_a)
+    }    
+    //% blockId=stopmp3 block="JQ8400 stop mp3"
+    //% weight=97
+    export function stopmp3() {
+            let _a=pins.createBuffer(4)
+    	       _a[0] = 170
+            _a[1] = 4
+            _a[2] = 0
+            _a[3] = 174
+    	       serial.writeBuffer(_a)
+    }
+    //% blockId=previousmp3 block="JQ8400 play previous mp3"
+    //% weight=98
+    export function previousmp3() {
+            let _a=pins.createBuffer(4)
+    	       _a[0] = 170
+            _a[1] = 5
+            _a[2] = 0
+            _a[3] = 175
+    	       serial.writeBuffer(_a)
+    }
+    //% blockId=nextmp3 block="JQ8400 play next mp3"
+    //% weight=99
+    export function nextmp3() {
+            let _a=pins.createBuffer(4)
+    	       _a[0] = 170
+            _a[1] = 6
+            _a[2] = 0
+            _a[3] = 176
+    	       serial.writeBuffer(_a)
+    }  
+    //% blockId=mp3play block="JQ8400 play mp3 %id "
+    //% weight=110
+   //% id.min=1 id.defl=1
+    export function mp3play(id:number) {
+            let _a=pins.createBuffer(6)
+    	       _a[0] = 170
+            _a[1] = 7
+            _a[2] = 2
+            _a[3] = 0
+            _a[4] = id
+            _a[5] = 179+id       
+    	       serial.writeBuffer(_a)
+    }
+
+    //% blockId=setmp3vol block="JQ8400 set mp3 volumn %vol "
+    //% weight=109
+    //% vol.min=0 vol.max=30  vol.defl=0
+    export function setmp3vol(vol:number) {
+            let _a=pins.createBuffer(5)
+    	       _a[0] = 170
+            _a[1] = 19
+            _a[2] = 1
+            _a[3] = vol
+            _a[4] = 190+vol       
+    	       serial.writeBuffer(_a)
+    }
+
+    //% blockId=insertmp3 block="JQ8400 insert mp3  %id "
+    //% weight=106
+    //% id.min=1 id.defl=1
+    export function insertmp3(id:number) {
+            let _a=pins.createBuffer(7)
+    	       _a[0] = 170
+            _a[1] = 22
+            _a[2] = 3
+            _a[3] = 2
+            _a[4] = 0
+            _a[5] = id
+            _a[6] = 197+id       
+    	       serial.writeBuffer(_a)
+    }
+
+    //% blockId=mp3mode block="JQ8400 mp3 play mode  %id "
+    //% weight=108
+    export function mp3mode(id:play_mode) {
+        let _a=pins.createBuffer(5)
+    	   _a[0] = 170
+        _a[1] = 24
+        _a[2] = 1
+        if (id==1)
+        {
+            _a[3] = 0
+            _a[4] = 195     
+        }else if (id==2)
+        {
+            _a[3] = 1
+            _a[4] = 196     
+        }else if (id==3)
+        {
+            _a[3] = 2
+            _a[4] = 197     
+        }else if (id==4)
+        {
+            _a[3] = 3
+            _a[4] = 198    
+        }else if (id==5)
+        {
+            _a[3] = 7
+            _a[4] = 202    
+        }
+   	   serial.writeBuffer(_a)
+    }
+
+     //% blockId=playmp3mode block="JQ8400 mp3 %id %sec sec"
+    //% weight=107
+    export function playmp3mode(id:mp3_mode,sec:number) {
+        let _a=pins.createBuffer(6)
+    	   _a[0] = 170
+        _a[2] = 2
+        _a[3]=sec/256
+        _a[4]=sec%256
+         if (id==1)
+         {
+              _a[1] = 35
+              _a[5]=207+_a[4]
+         }else if (id==2)
+         {
+              _a[1] = 34
+              _a[5]=206+_a[4]
+         }
+         serial.writeBuffer(_a)
+    }
+     //% blockId=checkmp3status block="JQ8400 play now?"
+    //% weight=103
+    export function checkmp3status():boolean {
+         let _a = pins.createBuffer(4)
+         _a[0] = 170
+         _a[1] = 1
+         _a[2] = 0
+         _a[3] = 171
+         serial.writeBuffer(_a)
+         basic.pause(100)
+         let _b = serial.readBuffer(5)
+         let _c = _b.getNumber(NumberFormat.UInt8LE, 3)
+         if (_c==1)
+              return true
+         else
+              return false
+    }
+}
